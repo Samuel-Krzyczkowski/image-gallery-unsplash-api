2b308d69f5369b16c54a63943c92ee653c35b60a
samekwerty <samcio90@gmail.com>
2022-09-15 18:39:51 +0200

    Updated package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
+module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
